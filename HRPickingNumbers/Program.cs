946efe943bc9dfa39d18a203149d4d6fe9c4cdaf
Jeremy <JeremyPlott@gmail.com>
2019-10-24 15:03:11 -0400

    solved for all ints same # by taking max val as default return value

--- HRPickingNumbers/Program.cs
+++ HRPickingNumbers/Program.cs
@@ -16,8 +16,33 @@ namespace HRPickingNumbers {
     class Program {
         static void Main(string[] args) {
 
+            List<int> a = new List<int>() { 4, 97, 5, 97, 97, 4, 97, 4, 97, 97, 97, 97, 4, 4, 5, 5, 97, 5, 97, 99, 4, 97, 5, 97, 
+                97, 97, 5, 5, 97, 4, 5, 97, 97, 5, 97, 4, 97, 5, 4, 4, 97, 5, 5, 5, 4, 97, 97, 4, 97, 5, 4, 4, 97, 97, 97, 5, 5, 
+                97, 4, 97, 97, 5, 4, 97, 97, 4, 97, 97, 97, 5, 4, 4, 97, 4, 4, 97, 5, 97, 97, 97, 97, 4, 97, 5, 97, 5, 4, 97, 4, 
+                5, 97, 97, 5, 97, 5, 97, 5, 97, 97, 97 };
 
+            SortedDictionary<int, int> d = new SortedDictionary<int, int>();
 
+            foreach(int i in a) {
+                if(!d.ContainsKey(i)) {
+                    d.Add(i, 1);
+                } else {
+                    d[i]++;
+                }
+            }
+
+            var largest = d.Values.Max();
+
+            foreach(var key in d.Keys) {                
+                var count = 0;
+                if(d.ContainsKey(key + 1)) {
+                    count = d[key] + d[key + 1];
+                }
+                if(largest < count) {
+                    largest = count;
+                }
+            }
+            Console.WriteLine(largest);
         }
     }
 }
